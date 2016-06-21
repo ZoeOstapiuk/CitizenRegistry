8fc9c7cca818770c1c421b189d08c5e324c89907
ZoeOstapiuk <ms.ostapyuk@mail.ru>
2016-06-21 12:02:36 +0300

    Changed loops and few details

--- Citizens/CitizenRegistry.cs
+++ Citizens/CitizenRegistry.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using Humanizer;
 
 namespace Citizens
@@ -11,11 +8,13 @@ namespace Citizens
     {
         private readonly DateTime dateToCompare = new DateTime(1899, 12, 31);
         private ICitizen[] allCitizens;
-        private DateTime lastRegister;
+        private int registeredCitizensCount;
+        private DateTime lastRegisterTime;
 
         public CitizenRegistry()
         {
             allCitizens = new ICitizen[0];
+            registeredCitizensCount = 0;
         }
 
         public ICitizen this[string id]
@@ -49,27 +48,21 @@ namespace Citizens
                     }
 
                     AddCitizenToArray(citizen);
-                    lastRegister = SystemDateTime.Now();
                     return;
                 }
             }
 
             citizen.VatId = GetID(citizen);
             AddCitizenToArray(citizen);
-            lastRegister = SystemDateTime.Now();
         }
 
         public string Stats()
         {
             int female = 0;
             int male = 0;
-            foreach (var person in allCitizens)
+            for (int i = 0; i < registeredCitizensCount; i++)
             {
-                if (person == null)
-                {
-                    break;
-                }
-                else if (person.Gender == Gender.Female)
+                if (allCitizens[i].Gender == Gender.Female)
                 {
                     female++;
                 }
@@ -85,7 +78,7 @@ namespace Citizens
             }
 
             return "man".ToQuantity(male) + " and " + "woman".ToQuantity(female)
-            + ". Last registration was " + lastRegister.Humanize(dateToCompareAgainst: SystemDateTime.Now());
+            + ". Last registration was " + lastRegisterTime.Humanize(dateToCompareAgainst: SystemDateTime.Now());
         }
 
         private string GetID(ICitizen citizen)
@@ -95,31 +88,24 @@ namespace Citizens
                 throw new ArgumentNullException("citizen");
             }
 
-            string result = String.Format("{0:00000}", citizen.BirthDate.Subtract(dateToCompare).TotalDays);
+            var result = new StringBuilder();
+            result.Append(String.Format("{0:00000}", citizen.BirthDate.Subtract(dateToCompare).TotalDays));
+
             int? ordinalNumber = null;
-            foreach (var person in allCitizens)
+            for (int i = registeredCitizensCount - 1; i >= 0; i--)
             {
-                if (person == null)
+                if (allCitizens[i].VatId.StartsWith(result.ToString()))
                 {
+                    ordinalNumber = Convert.ToInt32(allCitizens[i].VatId.Substring(6, 4));
                     break;
-                }
-                else if ((person.VatId.StartsWith(result) && ordinalNumber.HasValue && 
-                         ordinalNumber < Convert.ToInt32(person.VatId.Substring(6, 4))) ||
-                         (person.VatId.StartsWith(result) && !ordinalNumber.HasValue))
-                {
-                    ordinalNumber = Convert.ToInt32(person.VatId.Substring(6, 4));
                 }
             }
 
-            result += String.Format("{0:0000}", ordinalNumber != null ? ordinalNumber + 2 :
-                      (citizen.Gender == Gender.Male ? 1 : 0));
-            int sum = Convert.ToByte(result[0].ToString()) * (-1) + Convert.ToByte(result[1].ToString()) * 5 +
-                      Convert.ToByte(result[2].ToString()) * 7 + Convert.ToByte(result[3].ToString()) * 9 +
-                      Convert.ToByte(result[4].ToString()) * 4 + Convert.ToByte(result[5].ToString()) * 6 +
-                      Convert.ToByte(result[6].ToString()) * 10 + Convert.ToByte(result[7].ToString()) * 5 +
-                      Convert.ToByte(result[8].ToString()) * 7;
-            result += (sum % 11) % 10;
-            return result;
+            result.Append(String.Format("{0:0000}", ordinalNumber != null ? ordinalNumber + 2 :
+                         (citizen.Gender == Gender.Male ? 1 : 0)));
+            result.Append(FindKey(result.ToString()));
+
+            return result.ToString();
         }
 
         private void AddCitizenToArray(ICitizen citizen)
@@ -134,22 +120,24 @@ namespace Citizens
                 Array.Resize<ICitizen>(ref allCitizens, 1);
             }
 
-            ICitizen current;
-            for (int i = 0; i < allCitizens.Length; i++)
+            if (registeredCitizensCount == allCitizens.Length)
             {
-                if (allCitizens[i] == null)
-                {
-                    current = new Citizen(citizen.FirstName, citizen.LastName, citizen.BirthDate, citizen.Gender);
-                    current.VatId = citizen.VatId;
-                    allCitizens[i] = current;
-                    return;
-                }
+                Array.Resize<ICitizen>(ref allCitizens, allCitizens.Length * 2);
             }
 
-            Array.Resize<ICitizen>(ref allCitizens, allCitizens.Length * 2);
-            current = new Citizen(citizen.FirstName, citizen.LastName, citizen.BirthDate, citizen.Gender);
-            current.VatId = citizen.VatId;
-            allCitizens[allCitizens.Length / 2] = current;
+            allCitizens[registeredCitizensCount] = new Citizen(citizen.FirstName, citizen.LastName, citizen.BirthDate, citizen.Gender);
+            allCitizens[registeredCitizensCount].VatId = citizen.VatId;
+            lastRegisterTime = SystemDateTime.Now();
+            registeredCitizensCount++;
+        }
+
+        private char FindKey(string preId)
+        {
+            return (((Convert.ToByte(preId[0].ToString()) * (-1) + Convert.ToByte(preId[1].ToString()) * 5 +
+                   Convert.ToByte(preId[2].ToString()) * 7 + Convert.ToByte(preId[3].ToString()) * 9 +
+                   Convert.ToByte(preId[4].ToString()) * 4 + Convert.ToByte(preId[5].ToString()) * 6 +
+                   Convert.ToByte(preId[6].ToString()) * 10 + Convert.ToByte(preId[7].ToString()) * 5 +
+                   Convert.ToByte(preId[8].ToString()) * 7) % 11) % 10).ToString()[0];
         }
     }
 }
